f5e77d906e70f80981c9598cecab6e7b0577ba43
jithu291 <sreejithjithu291@gmail.com>
2024-02-28 16:41:17 +0530

    login

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
+import Login from './Pages/Login'
 
 function App() {
  
 
   return (
     <div>
-      <h1>Skill Share</h1>
+<Login/>
     </div>
    
   )
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import loginImg from '../assets/Images/login.png'
+function Login() {
+  return (
+    <div className='bg-image'>
+        <div className='color-div row border rounded shadow p-5 ' style={{ width:'80%', height:'70%'}}>
+    <div className='col-lg-6 ' >
+        <h1 className='text-center' style={{marginTop:'-35px'}} >Welcome <span style={{color:'red'}}>Back</span> </h1>
+        <img className='mt-4' style={{height:'90%', width:'100%'}} src={loginImg} alt="" />
+    </div>
+    <div className='col-lg-6 mt-5 '>
+    <Form>
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="floatingEmail" placeholder="Enter email" />
+       
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="floatingPassword" placeholder="Password" />
+      </Form.Group>
+     
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+    </div>
+
+        </div>
+    
+       
+    </div>
+  )
+}
+
+export default Login
