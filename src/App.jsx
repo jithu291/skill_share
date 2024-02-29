2e3fa840c735bdada822c6cfd0241cece12c8e26
jithu291 <sreejithjithu291@gmail.com>
2024-02-29 12:00:48 +0530

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
 
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import Login from './Pages/Login'
 import Register from './Pages/Register'
+import Header from './Pages/Header'
 
 
 
@@ -9,11 +11,12 @@ function App() {
 
 
   return (
+<Routes>
+  <Route path='/login' element={<Login/>}/>
+  <Route path='/register' element={<Register/>}/>
+  <Route path='/header' element={<Header/>}/>
 
-
-    <div>
-<Register/>
-    </div>
+</Routes>
 
   )
 }
